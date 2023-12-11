2acbbd670495a250492ea980d5d67c0d1f5b08f8
Vladislav <bogomolleha7@gmail.com>
2023-12-12 02:02:31 +0700

    Добавлен класс "PriorityOrder"

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Model/PriorityOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Хранит данные о приоритетном заказе.
+    /// </summary>
+    public class PriorityOrder:Order
+    {
+        /// <summary>
+        /// Возвращает и задает желаемую дату доставки.
+        /// </summary>
+        public DateTime DesiredDeliveryDate { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает желаемое время доставки.
+        /// </summary>
+        public string DesiredDeliveryTime { get; set; }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="PriorityOrder"/>.
+        /// </summary>
+        public PriorityOrder(DateTime dateTime, string deliveryTime = "None") : base()
+        {
+            DesiredDeliveryDate = dateTime;
+            DesiredDeliveryTime = deliveryTime;
+            DeliveryAddress = new Address();
+        }
+    }
+}
