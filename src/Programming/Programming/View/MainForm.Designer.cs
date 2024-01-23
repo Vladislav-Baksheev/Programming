4ed90d6e08508d1eec66fa33efbcf4d7138302f4
Vladislav <bogomolleha7@gmail.com>
2024-01-23 16:17:33 +0700

    First Lab complete

--- src/Programming/Programming/View/MainForm.Designer.cs
+++ src/Programming/Programming/View/MainForm.Designer.cs
@@ -37,9 +37,20 @@
             EnumsLabel = new Label();
             tabControl1 = new TabControl();
             EnumsTabPage = new TabPage();
+            SeasonGroupBox = new GroupBox();
+            GoButton = new Button();
+            SeasonComboBox = new ComboBox();
+            label2 = new Label();
+            ParsingGroupBox = new GroupBox();
+            WeekdayLabel = new Label();
+            ParseButton = new Button();
+            ParseTextBox = new TextBox();
+            label1 = new Label();
             EnumsGroupBox.SuspendLayout();
             tabControl1.SuspendLayout();
             EnumsTabPage.SuspendLayout();
+            SeasonGroupBox.SuspendLayout();
+            ParsingGroupBox.SuspendLayout();
             SuspendLayout();
             // 
             // EnumsGroupBox
@@ -83,6 +94,7 @@
             ValueListBox.Name = "ValueListBox";
             ValueListBox.Size = new Size(145, 184);
             ValueListBox.TabIndex = 4;
+            ValueListBox.SelectedIndexChanged += ValueListBox_SelectedIndexChanged;
             // 
             // IntValueLabel
             // 
@@ -118,25 +130,116 @@
             tabControl1.Location = new Point(0, 0);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(800, 450);
+            tabControl1.Size = new Size(800, 538);
             tabControl1.TabIndex = 1;
             // 
             // EnumsTabPage
             // 
+            EnumsTabPage.BackColor = Color.Transparent;
+            EnumsTabPage.Controls.Add(SeasonGroupBox);
+            EnumsTabPage.Controls.Add(ParsingGroupBox);
             EnumsTabPage.Controls.Add(EnumsGroupBox);
             EnumsTabPage.Location = new Point(4, 24);
             EnumsTabPage.Name = "EnumsTabPage";
             EnumsTabPage.Padding = new Padding(3);
-            EnumsTabPage.Size = new Size(792, 422);
+            EnumsTabPage.Size = new Size(792, 510);
             EnumsTabPage.TabIndex = 0;
             EnumsTabPage.Text = "Enums";
-            EnumsTabPage.UseVisualStyleBackColor = true;
+            // 
+            // SeasonGroupBox
+            // 
+            SeasonGroupBox.Controls.Add(GoButton);
+            SeasonGroupBox.Controls.Add(SeasonComboBox);
+            SeasonGroupBox.Controls.Add(label2);
+            SeasonGroupBox.Location = new Point(361, 280);
+            SeasonGroupBox.Name = "SeasonGroupBox";
+            SeasonGroupBox.Size = new Size(397, 151);
+            SeasonGroupBox.TabIndex = 2;
+            SeasonGroupBox.TabStop = false;
+            SeasonGroupBox.Text = "Season Handle";
+            // 
+            // GoButton
+            // 
+            GoButton.Location = new Point(249, 49);
+            GoButton.Name = "GoButton";
+            GoButton.Size = new Size(75, 23);
+            GoButton.TabIndex = 2;
+            GoButton.Text = "Go!";
+            GoButton.UseVisualStyleBackColor = true;
+            GoButton.Click += GoButton_Click;
+            // 
+            // SeasonComboBox
+            // 
+            SeasonComboBox.FormattingEnabled = true;
+            SeasonComboBox.Location = new Point(7, 49);
+            SeasonComboBox.Name = "SeasonComboBox";
+            SeasonComboBox.Size = new Size(224, 23);
+            SeasonComboBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(7, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Choose season:";
+            // 
+            // ParsingGroupBox
+            // 
+            ParsingGroupBox.Controls.Add(WeekdayLabel);
+            ParsingGroupBox.Controls.Add(ParseButton);
+            ParsingGroupBox.Controls.Add(ParseTextBox);
+            ParsingGroupBox.Controls.Add(label1);
+            ParsingGroupBox.Location = new Point(3, 278);
+            ParsingGroupBox.Name = "ParsingGroupBox";
+            ParsingGroupBox.Size = new Size(352, 153);
+            ParsingGroupBox.TabIndex = 1;
+            ParsingGroupBox.TabStop = false;
+            ParsingGroupBox.Text = "Weekday Parsing";
+            // 
+            // WeekdayLabel
+            // 
+            WeekdayLabel.AutoSize = true;
+            WeekdayLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            WeekdayLabel.ForeColor = SystemColors.ButtonShadow;
+            WeekdayLabel.Location = new Point(6, 87);
+            WeekdayLabel.Name = "WeekdayLabel";
+            WeekdayLabel.Size = new Size(16, 15);
+            WeekdayLabel.TabIndex = 5;
+            WeekdayLabel.Text = "...";
+            // 
+            // ParseButton
+            // 
+            ParseButton.Location = new Point(261, 50);
+            ParseButton.Name = "ParseButton";
+            ParseButton.Size = new Size(75, 23);
+            ParseButton.TabIndex = 4;
+            ParseButton.Text = "Parse";
+            ParseButton.UseVisualStyleBackColor = true;
+            ParseButton.Click += ParseButton_Click;
+            // 
+            // ParseTextBox
+            // 
+            ParseTextBox.Location = new Point(6, 50);
+            ParseTextBox.Name = "ParseTextBox";
+            ParseTextBox.Size = new Size(235, 23);
+            ParseTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(125, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Type value for parsing:";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 538);
             Controls.Add(tabControl1);
             Name = "MainForm";
             Text = "Programming Demo";
@@ -144,6 +247,10 @@
             EnumsGroupBox.PerformLayout();
             tabControl1.ResumeLayout(false);
             EnumsTabPage.ResumeLayout(false);
+            SeasonGroupBox.ResumeLayout(false);
+            SeasonGroupBox.PerformLayout();
+            ParsingGroupBox.ResumeLayout(false);
+            ParsingGroupBox.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -158,5 +265,14 @@
         private TextBox IntValueTextBox;
         private TabControl tabControl1;
         private TabPage EnumsTabPage;
+        private GroupBox ParsingGroupBox;
+        private Label WeekdayLabel;
+        private Button ParseButton;
+        private TextBox ParseTextBox;
+        private Label label1;
+        private GroupBox SeasonGroupBox;
+        private Button GoButton;
+        private ComboBox SeasonComboBox;
+        private Label label2;
     }
 }
--- src/Programming/Programming/View/MainForm.cs
+++ src/Programming/Programming/View/MainForm.cs
@@ -6,6 +6,12 @@ public partial class MainForm : Form
     public MainForm()
     {
         InitializeComponent();
+        EnumsListBox.SelectedIndex = 0;
+        Array values = Enum.GetValues(typeof(Season));
+        foreach (var value in values)
+        {
+            SeasonComboBox.Items.Add(value.ToString());
+        }
     }
 
 
@@ -23,5 +29,43 @@ public partial class MainForm : Form
         }
 
         IntValueTextBox.Text = "";
+    }
+
+    private void ValueListBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        IntValueTextBox.Text = ((int)ValueListBox.SelectedItem).ToString();
+    }
+
+    private void ParseButton_Click(object sender, EventArgs e)
+    {
+        string text = ParseTextBox.Text;
+
+        if (Enum.TryParse(text, out Weekday weekday))
+        {
+            WeekdayLabel.Text = $"Это день недели ({text} = {(int)weekday})";
+        }
+        else
+        {
+            WeekdayLabel.Text = "Нет такого дня недели";
+        }
+    }
+
+    private void GoButton_Click(object sender, EventArgs e)
+    {
+        switch (SeasonComboBox.SelectedIndex)
+        {
+            case 0:
+                MessageBox.Show("Ура, солнце!");
+                break;
+            case 1:
+                EnumsTabPage.BackColor = System.Drawing.Color.Yellow;
+                break;
+            case 2:
+                EnumsTabPage.BackColor = System.Drawing.Color.Green;
+                break;
+            case 3:
+                MessageBox.Show("Брр, холодно!");
+                break;
+        }
     }
 }
