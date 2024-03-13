d30c6ceddeabdd9fa5fd8db5024427f5922521c5
Vladislav <bogomolleha7@gmail.com>
2024-03-14 02:28:05 +0700

    feature(Contacts): создан проект приложения на MVVM

---
+++ src/Contacts/View/Model/Contact.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    public class Contact
+    {
+        private string _name;
+
+        private string _phone;
+
+        private string _email;
+
+        public string Name 
+        { 
+            get 
+            { 
+                return _name; 
+            } 
+            set 
+            { 
+                _name = value; 
+            } 
+        }
+        public string Phone
+        {
+            get
+            {
+                return _phone;
+            }
+            set
+            {
+                _phone = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+        public Contact()
+        {
+            Name = string.Empty;
+            Phone = string.Empty;
+            Email = string.Empty;
+        }
+
+        public Contact(string name, string phone, string email)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+        }
+    }
+}
