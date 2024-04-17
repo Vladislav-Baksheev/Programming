02436275c5c0c67c3a8ae1772223f24d7a55acf7
Vladislav <bogomolleha7@gmail.com>
2024-04-17 22:53:44 +0700

    feature(ContactControl): Добавлен запрещающий ввод любых недопустимых символов в текстовое поле Phone

--- src/Contacts/View/ViewModel/Controls/ContactControl.xaml.cs
+++ src/Contacts/View/ViewModel/Controls/ContactControl.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,39 @@ namespace View.ViewModel
     /// </summary>
     public partial class ContactControl : UserControl
     {
+        /// <summary>
+        /// Допустимые символы.
+        /// </summary>
+        private static readonly Regex _regex = new Regex("[^0-9()+-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
         public ContactControl()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = "0123456789 ".IndexOf(e.Text) < 0;
+        }
+
+        private void TextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
         }
     }
 }
